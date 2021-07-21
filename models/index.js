873ac4c4f3ccb5c239477c7ace03c25eeeaf6a6e
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-07-21 19:20:51 -0400

    added to models index to connect to eachothers model association

--- models/index.js
+++ models/index.js
@@ -11,4 +11,22 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
+User.belongsToMany(Post, {
+    through: Vote, 
+    as: 'voted_posts', 
+    foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+    through: Vote, 
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Vote.
+
 module.exports = { User, Post };
