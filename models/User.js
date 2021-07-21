3e96eeb63d6e68f55c19917425c4fce9bd1f1330
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-07-20 20:21:40 -0400

    set up user api route

--- models/User.js
+++ models/User.js
@@ -8,6 +8,42 @@ class User extends Model {}
 User.init(
     {
         // Table column definitions
+        // define id column
+        id: {
+            // use the special Squelize DataTypes object provide what type
+            type: DataTypes.INTEGER,
+            // equal to SQL's 'NOT NULL' option
+            allowNull: false,
+            // instruct that this is the Primary key
+            primaryKey: true,
+            // turn on an auto increment
+            autoIncrement: true
+        },
+        // define a username column
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        //define email column
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            // no duplicate email values in this table
+            unique: true,
+            // if allowNull false we can run data through validate before goes to table
+            validate: {
+                isEmail: true
+            }
+        },
+        // define a password column
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                // this means the password has to be at least 4 characters long
+                len: [4]
+            }
+        }
     },
     {
 
