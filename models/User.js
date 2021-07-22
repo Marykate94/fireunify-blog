0a966179ae2903e12d0c5f19eb6e93cd34dadc1e
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-07-22 19:48:48 -0400

    added comment-route folder

--- models/User.js
+++ models/User.js
@@ -1,79 +1,61 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
 
-//create our User model
+// create our User model
 class User extends Model {
-    // set method to run on instance data (per user) to check password
-    checkPassword(loginPw) {
-        return bcrypt.compareSync(loginPw, this.password);
-    }
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
 }
 
-// define table columns and configuration
+// create fields/columns for User model
 User.init(
-    {
-        // Table column definitions
-        // define id column
-        id: {
-            // use the special Squelize DataTypes object provide what type
-            type: DataTypes.INTEGER,
-            // equal to SQL's 'NOT NULL' option
-            allowNull: false,
-            // instruct that this is the Primary key
-            primaryKey: true,
-            // turn on an auto increment
-            autoIncrement: true
-        },
-        // define a username column
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        //define email column
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            // no duplicate email values in this table
-            unique: true,
-            // if allowNull false we can run data through validate before goes to table
-            validate: {
-                isEmail: true
-            }
-        },
-        // define a password column
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                // this means the password has to be at least 4 characters long
-                len: [4]
-            }
-        }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
-    {
-        hooks: {
-            // set up beforeCreate lifecycle "hook functionality
-            async beforeCreate(newUserData) { 
-                newUserData.password = await bcrypt.hash(newUserData.password, 10);
-                return newUserData;
-            },
-            // set up beforeUpdate lifecycle "hook"
-            async beforeUpdate(updatedUserData) {
-                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-                return updatedUserData;
-            }
-        },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4]
+      }
+    }
+  },
+  {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
 
-    // pass in our imported sequelize connection (the direct connection to our database)
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
+    },
     sequelize,
-    // don't automatically create createdAt/updatedAt timestamp fields
     timestamps: false,
-    // don't pluralize name of database table
     freezeTableName: true,
-    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
     underscored: true,
-    // make it so our model name stays lowercase in the database
     modelName: 'user'
   }
 );
