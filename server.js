6ead8b5d64242e8a6987a1928d4cbe854eacd6ed
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-07-22 23:20:57 -0400

    added home-routes for login

--- server.js
+++ server.js
@@ -4,9 +4,20 @@ const routes = require('./controllers');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session');
+const sequelize = require('./config/connection.js');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false, 
+  saveUninitialized: true, 
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 // import sequelize connection
-
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -16,6 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.use(session(sess));
 
 app.use(routes);
 
