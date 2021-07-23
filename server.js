8376760bfc7028ae0becb7428724bc3cad6e0b6f
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-07-22 21:30:58 -0400

    added main handlebars

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const connection = require('./config/connection.js')
 const routes = require('./controllers');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 // import sequelize connection
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
