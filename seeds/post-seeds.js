d7de10fb0a2d954560b74cb593eea6544204027d
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-07-25 17:36:10 -0400

    added new color theme

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -2,22 +2,22 @@ const { Post } = require('../models');
 
 const postdata = [
   {
-    title: 'Donec posuere metus vitae ipsum.',
+    title: 'Goshawk news.',
     post_url: 'https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png',
     user_id: 10
   },
   {
-    title: 'Morbi non quam nec dui luctus rutrum.',
+    title: 'Redtail hawk.',
     post_url: 'https://nasa.gov/donec.json',
     user_id: 8
   },
   {
-    title: 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.',
+    title: 'Peregruine falcon.',
     post_url: 'https://europa.eu/parturient/montes/nascetur/ridiculus/mus/etiam/vel.aspx',
     user_id: 1
   },
   {
-    title: 'Nunc purus.',
+    title: 'Golden eagle.',
     post_url: 'http://desdev.cn/enim/blandit/mi.jpg',
     user_id: 4
   },
